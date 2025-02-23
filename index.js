9c64741d2eb34e7ab6376566d2917a961f8f3ce6
Shabirahmad1676 <sa3835746@gmail.com>
2025-02-23 22:36:58 +0500

    Learned about error handling middleware and req/res objects

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+
+  throw new Error('Something went wrong!');
+  
+  // console.log(req.method);
+  // console.log(req.url);
+  // console.log(req.headers);
+  // console.log(req.baseUrl);
+})
+
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(3000)
